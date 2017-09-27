2336512535dcf8c8cc737974908374b0b62dd1b0
Ravi Panchal <pancha.ravi@gmail.com>
2017-09-27 12:40:27 -0400

    Initial design of Index & LoginPage

---
+++ routes/user.js
@@ -0,0 +1,99 @@
+var express  = require('express');
+var passport = require('passport');
+var local    = require('passport-local');
+var bcrypt   = require('bcrypt');
+var models   = require('../models/index');
+var User     = models.user;
+var router   = express.Router();
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// router.get('/login', function(req, res, next) {
+//   res.render('users/index');
+// });
+
+// Passport.
+passport.use(
+	new local.Strategy(
+		function(email, password, done) {
+	    User.findOne({
+				where: {
+					email: email
+				}
+			}).then(function(user) {
+	      if (!user)
+	        return(done(null, false, {message: 'A user with that email does not exist.'}));
+	      else {
+					bcrypt.compare(password, user.password, function(error, result) {
+						if (result)
+				      return(done(null, user));
+						else
+			        return(done(null, false, {message: 'Incorrect password.'}));
+	        	});
+			}
+	    });
+	  }
+	)
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id).then(function(user) {
+    done(null, user);
+  });
+});
+
+// Sign up.
+router.get('/signup', function(request, response) {
+	response.render('user/signup', {
+		user: {}
+	})
+});
+
+router.post('/signup', function(request, response) {
+	bcrypt.hash(request.body.password, 10, function(error, password) {
+		console.log("I am here for Checking Signup Loop")
+		User.create({
+			email:    request.body.email,
+			password: password,
+			name:     request.body.name
+		}).then(function(user) {
+			request.login(user, function(error) {
+				response.redirect('/');
+			});
+		}).catch(function(error) {
+			response.render('user/signup', {
+				user:   request.body,
+				errors: error.errors
+			});
+		});
+	});
+});
+
+// Log in.
+router.get('/login', function(request, response) {
+	console.log("Login Loop");
+	response.render('user/index')
+});
+
+router.post('/login', passport.authenticate('local'), function(request, response) {
+	console.log("Login Loop 1");
+	response.redirect('/imagepost');
+});
+
+// Log out.
+router.get('/logout', function(request, response) {
+	request.logout();
+	response.redirect('/');
+});
+
+
+
+
+module.exports = router;
