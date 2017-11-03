0088c7bc4a678177a79a7e0445b846fb069f6a79
Ravi Panchal <pancha.ravi@gmail.com>
2017-11-03 14:20:46 -0400

    updated the students route with post method to insert the student data in students table

--- models/students.js
+++ models/students.js
@@ -6,45 +6,89 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
         notEmpty: {
           msg: 'Please enter firstname'
-        },
+        }
+      }
     },
     lastname: {
       type:      DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: {
-          msg: 'Please enter lastname'
-        },
-      }
+      allowNull: false
     },
     parentname: {
       type:      DataTypes.STRING,
-      allowNull: false,
+      allowNull: true
+    },
+    address: {
+      type:      DataTypes.STRING,
+      allowNull: true,
       validate: {
         notEmpty: {
-          msg: 'Please enter parent name'
+          msg: 'Please enter address'
         }
       }
-    }
-    parentname: {
+    },
+    village: {
       type:      DataTypes.STRING,
       allowNull: false,
       validate: {
         notEmpty: {
-          msg: 'Please enter parent name'
+          msg: 'Please enter village name'
         }
       }
+    },
+    dob: {
+      type:      DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Please Enter DOB'
+        }
+      }
+    },
+    gender: {
+      type:      DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Please select Gender'
+        }
+      }
+    },
+    income: {
+      type:      DataTypes.INTEGER,
+      allowNull: true
+    },
+    aadharnbr: {
+      type:      DataTypes.STRING,
+      allowNull: true
+    },
+    housetype: {
+      type:      DataTypes.STRING,
+      allowNull: true
+    },
+    interest: {
+      type:      DataTypes.STRING,
+      allowNull: true
+    },
+    financialposition: {
+      type:      DataTypes.STRING,
+      allowNull: true
+    },
+    studycommitment: {
+      type:      DataTypes.STRING,
+      allowNull: false
+    },
+    total: {
+      type:      DataTypes.STRING,
+      allowNull: true
+    },
+    activity: {
+      type:      DataTypes.STRING,
+      allowNull: true
+    },
+    comments: {
+      type:      DataTypes.STRING,
+      allowNull: true
     }
-    
-  }
-  //   {
-  //   classMethods: {
-  //     associate: function(models) {
-  //       models.user.hasMany(models.imagepost);
-  //     }
-  //   }
-  // }
-  )
-
-  return(User);
+  })
+  return(Student);
 };
--- routes/student.js
+++ routes/student.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var models  = require('../models/index');
+var Student = models.student;
 var router = express.Router();
 
 /* GET for unauthorized users  */
@@ -9,6 +11,58 @@ router.get('/addstudent', function(req, res, next) {
     console.log("on student page");
     res.render('student/addstudent',{ title :' express student page'});
 
+});
+
+
+
+//Add Student Data
+router.post('/addstudent', function(request, response) {
+	console.log("In Add Student POST Method");
+	console.log(Student);
+	console.log(request.body.firstname);
+	Student.create({
+		firstname: request.body.firstname,
+		lastname: request.body.lastname,
+		dob: request.body.dob,
+		parentname: request.body.parentname,
+		address: request.body.address,
+		village: request.body.village,
+		gender: request.body.gender,
+		interest: request.body.interest,
+		aadharnbr: request.body.aadharnbr,
+		activity: request.body.activity,
+		financialposition: request.body.financialposition,
+		studycommitment: 'commited',
+		comment: request.body.comment,
+		income:request.body.income,
+		housetype:request.body.housetype,
+		total:request.body.total
+		// firstname: 'ravi',
+		// lastname: 'panchal',
+		// dob: '09/13/1980',
+		// parentname: 'sureshbhai',
+		// address: '777 Hartley dr.,',
+		// village: 'Ahmedabad',
+		
+		// interest: 'request.body.interest',
+		// aadharnbr: 'request.body.aadharnbr',
+		// activity: 'request.body.activity',
+		// financialposition: 'request.body.financialposition',
+		// studycommitment: 'request.body.studycommitment',
+		// comment: 'request.body.comment',
+		// income:'request.body.income',
+		// housetype:'request.body.housetype',
+		// total:'request.body.total'
+	}).then(function(student) {
+		console.log("Student Added");
+		response.redirect(post.url);
+	}).catch(function(error) {
+		console.log('NOTE: Student was not added');
+		response.render('student/addstudent', {
+			student: request.body,
+			errors:  error.errors
+		})
+	})	
 });
 
 
